bc38856c259f269ec6e8928d3bfe2cd66d70a902
SomiZ <116781266+SomiZ@users.noreply.github.com>
2025-02-01 20:59:27 -0500

    Create problem_4.py

---
+++ session_02/problem_4.py
@@ -0,0 +1,28 @@
+"""
+Created on Sat Feb  1 19:48:06 2025
+
+@author: somai
+(While Loop Version): Multiplication Table for Numbers 1-9
+Explanation:
+We use two while loops:
+The outer loop iterates through numbers 1 to 9 (for which we generate tables).
+The inner loop iterates from 1 to 10 to print the multiplication table for each number.
+The output is formatted to display each table clearly.
+Instructions:
+1. Initialize a variable (num) for the multiplication table starting from 1.
+2. Use a while loop to iterate num from 1 to 9.
+3. Inside this loop, use another while loop for numbers 1 to 10.
+4. Print each multiplication result, then increment counters properly.
+5. If you have time implemnt it with for loop as well/
+"""
+# Initialize outer loop counter
+
+# Outer loop: Iterate through numbers 1 to 9
+
+# Initialize inner loop counter
+
+# Inner loop: Iterate from 1 to 10
+
+    # Increment inner loop counter
+
+# Increment outer loop counter
