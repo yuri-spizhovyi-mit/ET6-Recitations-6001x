5b33494e12e9318601b2a8b39a25461353b09488
SomiZ <116781266+SomiZ@users.noreply.github.com>
2025-02-01 20:11:25 -0500

    Create problem_2.py

---
+++ session_02/problem_2.py
@@ -0,0 +1,22 @@
+"""
+Created on Sat Feb  1 19:48:06 2025
+
+@author: somai
+
+Problem 2: Checking for a Palindrome
+Explanation:
+A palindrome is a word or phrase that reads the same forward and backward (e.g., "madam", "racecar"). We can check for palindromes using string slicing and conditionals.
+
+Instructions:
+1. Ask the user to enter a word.
+2. Convert the word to lowercase to ensure case insensitivity.
+3. Check if the word is the same when reversed.
+4. Print "It's a palindrome!" if true, otherwise print "Not a palindrome."
+"""
+## Get user input
+
+
+# Convert to lowercase for case insensitivity by cally lower() function 
+
+# Check if the word is the same forward and backward
+
