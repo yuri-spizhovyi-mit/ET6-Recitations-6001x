4a952344a1c96b82037c1019163243b1a1312799
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-02-02 23:12:18 -0800

    add problem 4 session 2

--- session_02/problem_4.py
+++ session_02/problem_4.py
@@ -13,16 +13,25 @@ Instructions:
 2. Use a while loop to iterate num from 1 to 9.
 3. Inside this loop, use another while loop for numbers 1 to 10.
 4. Print each multiplication result, then increment counters properly.
-5. If you have time implemnt it with for loop as well/
+5. If you have time implement it with for loop as well/
 """
 # Initialize outer loop counter
-
+outer_counter = 1
 # Outer loop: Iterate through numbers 1 to 9
-
+while outer_counter <= 10:
 # Initialize inner loop counter
-
+    inner_counter = 1
 # Inner loop: Iterate from 1 to 10
-
+    while inner_counter <= 10:
+        result = outer_counter * inner_counter
+        print(f"While Solution: {outer_counter} * {inner_counter} = {result}")
     # Increment inner loop counter
-
+        inner_counter += 1
 # Increment outer loop counter
+    outer_counter +=1
+
+outer_for_counter = 1
+for i in range(1, 11):
+    for j in range(1, 11):
+        result = i * j
+        print(f"For Solution: {i} * {j} = {result}")
