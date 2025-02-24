ccf68d50a26b2c14a83e79af672fb35feb52b74d
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-02-24 07:17:28 -0800

    add list task

--- session_05_extra_midterm/f.py
+++ session_05_extra_midterm/f.py
@@ -1,8 +1,12 @@
-def f(x):
-    a = []
-    while x > 0:
-        a.append(x)
-        f(x - 1)
+def has22(nums):
+    result = 0
+    for i in range(len(nums) - 1):
+        if nums[i] == 2 and nums[i + 1] == 2:
+            result += 1
+    if result >= 1:
+        return True
+    else:
+        return False
 
 
-print(f(3))
+print(has22([1, 2, 2]))
