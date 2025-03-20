89ba74eb23620bf3ca89406ce3d201aace6dfe47
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-03-20 01:10:36 -0700

    add solution 2 for problem 3

--- session_10_finalExam_study/exercise.py
+++ session_10_finalExam_study/exercise.py
@@ -1,11 +1,10 @@
 def cipher(map_from, map_to, code):
     key_code = {}
-    decoded_chars = []
+    decoded_word = ""
     for i in range(len(map_from)):
         key_code[map_from[i]] = map_to[i]
     for char in code:
-        decoded_chars.append(key_code[char])
-    decoded_word = "".join(decoded_chars)
+        decoded_word += key_code[char]
     return key_code, decoded_word
 
 
